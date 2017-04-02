afc47d7250f914f577583be0ce1b8b3173db44e8
Ian Meyers <meyers.i@husky.neu.edu>
2017-04-01 20:06:11 -0400

    Abstracted things properly

--- src/FE7/units/AUnit.java
+++ src/FE7/units/AUnit.java
@@ -1,10 +1,191 @@
 package FE7.units;
 
+import java.util.Random;
+
 /**
  * Created by Ian on 3/31/2017.
  */
-public class AUnit {
+public abstract class AUnit implements FE7Unit {
 
-  String name;
-  int lvl;
+  public String name;
+  public String type;
+  public int lvl;
+  public int hp;
+  public int strMag;
+  public int skl;
+  public int spd;
+  public int def;
+  public int lck;
+  public int res;
+  public int con;
+  public int mov;
+  public int hpGrowth;
+  public int strMagGrowth;
+  public int sklGrowth;
+  public int spdGrowth;
+  public int defGrowth;
+  public int lckGrowth;
+  public int resGrowth;
+
+  @Override
+  public void levelUp() {
+    //Random r = new Random();
+    if (lvl < 20) {
+      this.lvl++;
+      this.hpUp();
+      this.strMagUp();
+      this.spdUp();
+      this.sklUp();
+      this.defUp();
+      this.resUp();
+      this.lckUp();
+    }
+    else {
+      throw new IllegalStateException("Can't level up a max level unit.");
+    }
+  }
+
+  @Override
+  public void hpUp() {
+    Random r = new Random();
+    if (r.nextInt(101) < hpGrowth) {
+      this.hp++;
+    }
+  }
+
+  @Override
+  public void strMagUp() {
+    Random r = new Random();
+    if (r.nextInt(101) < strMagGrowth) {
+      this.strMag++;
+    }
+  }
+
+  @Override
+  public void sklUp() {
+    Random r = new Random();
+    if (r.nextInt(101) < sklGrowth) {
+      this.skl++;
+    }
+  }
+
+  @Override
+  public void spdUp() {
+    Random r = new Random();
+    if (r.nextInt(101) < spdGrowth) {
+      this.spd++;
+    }
+  }
+
+  @Override
+  public void defUp() {
+    Random r = new Random();
+    if (r.nextInt(101) < defGrowth) {
+      this.def++;
+    }
+  }
+
+  @Override
+  public void lckUp() {
+    Random r = new Random();
+    if (r.nextInt(101) < lckGrowth) {
+      this.lck++;
+    }
+  }
+
+  @Override
+  public void resUp() {
+    Random r = new Random();
+    if (r.nextInt(101) < resGrowth) {
+      this.res++;
+    }
+  }
+
+  @Override
+  public abstract void promote();
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public int getLvl() {
+    return this.lvl;
+  }
+
+  @Override
+  public int getHp() {
+    return this.hp;
+  }
+
+  @Override
+  public int getStrMag() {
+    return this.strMag;
+  }
+
+  @Override
+  public int getSkl() {
+    return this.skl;
+  }
+
+  @Override
+  public int getSpd() {
+    return this.spd;
+  }
+
+  @Override
+  public int getDef() {
+    return this.def;
+  }
+
+  @Override
+  public int getLck() {
+    return this.lck;
+  }
+
+  @Override
+  public int getRes() {
+    return this.res;
+  }
+
+  @Override
+  public int getCon() {
+    return this.con;
+  }
+
+  @Override
+  public int getMov() {
+    return this.mov;
+  }
+
+  @Override
+  public String getType() {
+    return this.type;
+  }
+
+  @Override
+  public void display() {
+    System.out.println(this.name);
+    System.out.println(this.type);
+    System.out.println("LVL: " + this.lvl);
+    System.out.println("HP:  " + this.hp);
+    System.out.println("STR: " + this.strMag);
+    System.out.println("SKL: " + this.skl);
+    System.out.println("SPD: " + this.spd);
+    System.out.println("DEF: " + this.spd);
+    System.out.println("LCK: " + this.lck);
+    System.out.println("RES: " + this.res);
+    System.out.println("CON: " + this.con);
+    System.out.println("MOV: " + this.mov);
+  }
+
+  @Override
+  public void levelUpTo(int lvl) {
+    int x = lvl - this.lvl;
+    while(x > 0) {
+      this.levelUp();
+      x--;
+    }
+  }
 }
--- src/FE7/units/cavaliers/Sain.java
+++ src/FE7/units/cavaliers/Sain.java
@@ -2,32 +2,13 @@ package FE7.units.cavaliers;
 
 import java.util.Random;
 
+import FE7.units.AUnit;
 import FE7.units.FE7Unit;
 
 /**
  * Represents Sain from FE7.
  */
-public class Sain implements FE7Unit {
-
-  private String name;
-  private String type;
-  private int lvl;
-  private int hp;
-  private int strMag;
-  private int skl;
-  private int spd;
-  private int def;
-  private int lck;
-  private int res;
-  private int con;
-  private int mov;
-  private int hpGrowth;
-  private int strMagGrowth;
-  private int sklGrowth;
-  private int spdGrowth;
-  private int defGrowth;
-  private int lckGrowth;
-  private int resGrowth;
+public class Sain extends AUnit {
 
   /**
    * Constructs an instance of Sain.
@@ -55,80 +36,6 @@ public class Sain implements FE7Unit {
   }
 
   @Override
-  public void levelUp() {
-    Random r = new Random();
-    if (lvl < 20) {
-      this.lvl++;
-      this.hpUp();
-      this.strMagUp();
-      this.spdUp();
-      this.sklUp();
-      this.defUp();
-      this.resUp();
-      this.lckUp();
-    }
-    else {
-      throw new IllegalStateException("Can't level up a max level unit.");
-    }
-  }
-
-  @Override
-  public void hpUp() {
-    Random r = new Random();
-    if (r.nextInt(101) < hpGrowth) {
-      this.hp++;
-    }
-  }
-
-  @Override
-  public void strMagUp() {
-    Random r = new Random();
-    if (r.nextInt(101) < strMagGrowth) {
-      this.strMag++;
-    }
-  }
-
-  @Override
-  public void sklUp() {
-    Random r = new Random();
-    if (r.nextInt(101) < sklGrowth) {
-      this.skl++;
-    }
-  }
-
-  @Override
-  public void spdUp() {
-    Random r = new Random();
-    if (r.nextInt(101) < spdGrowth) {
-      this.spd++;
-    }
-  }
-
-  @Override
-  public void defUp() {
-    Random r = new Random();
-    if (r.nextInt(101) < defGrowth) {
-      this.def++;
-    }
-  }
-
-  @Override
-  public void lckUp() {
-    Random r = new Random();
-    if (r.nextInt(101) < lckGrowth) {
-      this.lck++;
-    }
-  }
-
-  @Override
-  public void resUp() {
-    Random r = new Random();
-    if (r.nextInt(101) < resGrowth) {
-      this.res++;
-    }
-  }
-
-  @Override
   public void promote() {
     if (this.lvl < 10) {
       throw new IllegalStateException("Unit must be at least level 10 to promote.");
@@ -147,91 +54,6 @@ public class Sain implements FE7Unit {
       this.res = res + 1;
       this.con = con + 2;
       this.mov = mov + 1;
-    }
-  }
-
-  @Override
-  public String getName() {
-    return this.name;
-  }
-
-  @Override
-  public int getLvl() {
-    return this.lvl;
-  }
-
-  @Override
-  public int getHp() {
-    return this.hp;
-  }
-
-  @Override
-  public int getStrMag() {
-    return this.strMag;
-  }
-
-  @Override
-  public int getSkl() {
-    return this.skl;
-  }
-
-  @Override
-  public int getSpd() {
-    return this.spd;
-  }
-
-  @Override
-  public int getDef() {
-    return this.def;
-  }
-
-  @Override
-  public int getLck() {
-    return this.lck;
-  }
-
-  @Override
-  public int getRes() {
-    return this.res;
-  }
-
-  @Override
-  public int getCon() {
-    return this.con;
-  }
-
-  @Override
-  public int getMov() {
-    return this.mov;
-  }
-
-  @Override
-  public String getType() {
-    return this.type;
-  }
-
-  @Override
-  public void display() {
-    System.out.println(this.name);
-    System.out.println(this.type);
-    System.out.println("LVL: " + this.lvl);
-    System.out.println("HP:  " + this.hp);
-    System.out.println("STR: " + this.strMag);
-    System.out.println("SKL: " + this.skl);
-    System.out.println("SPD: " + this.spd);
-    System.out.println("DEF: " + this.spd);
-    System.out.println("LCK: " + this.lck);
-    System.out.println("RES: " + this.res);
-    System.out.println("CON: " + this.con);
-    System.out.println("MOV: " + this.mov);
-  }
-
-  @Override
-  public void levelUpTo(int lvl) {
-    int x = lvl - this.lvl;
-    while(x > 0) {
-      this.levelUp();
-      x--;
     }
   }
 }
